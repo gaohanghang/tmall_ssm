aa02cc537567ea3bb87c4311374ecf916f13a180
gaohanghang <1341947277@qq.com>
2018-02-28 15:34:38 +0800

    SSM模仿天猫项目

--- src/main/resources/tmall_ssm.sql
+++ src/main/resources/tmall_ssm.sql
@@ -29,13 +29,13 @@ CREATE TABLE property (
 -- 产品表    存放产品信息，如LED40EC平板电视机，海尔EC6005热水器
 CREATE TABLE product (
   id int(11) NOT NULL AUTO_INCREMENT,
-  name varchar(255) DEFAULT NULL,
-  subTitle varchar(255) DEFAULT NULL,
-  originalPrice float DEFAULT NULL,
-  promotePrice float DEFAULT NULL,
-  stock int(11) DEFAULT NULL,
-  cid int(11) DEFAULT NULL,
-  createDate datetime DEFAULT NULL,
+  name varchar(255) DEFAULT NULL COMMENT '产品名称',
+  subTitle varchar(255) DEFAULT NULL COMMENT '小标题',
+  originalPrice float DEFAULT NULL COMMENT '原始价格',
+  promotePrice float DEFAULT NULL COMMENT '优惠价格',
+  stock int(11) DEFAULT NULL COMMENT '库存',
+  cid int(11) DEFAULT NULL COMMENT '外键 指向分类表的id字段',
+  createDate datetime DEFAULT NULL COMMENT '创建日期',
   PRIMARY KEY (id),
   CONSTRAINT fk_product_category FOREIGN KEY (cid) REFERENCES category (id)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
@@ -43,8 +43,8 @@ CREATE TABLE product (
 -- 属性值表   	存放属性值信息，如重量是900g,颜色是粉红色
 CREATE TABLE propertyvalue (
   id int(11) NOT NULL AUTO_INCREMENT,
-  pid int(11) DEFAULT NULL,
-  ptid int(11) DEFAULT NULL,
+  pid int(11) DEFAULT NULL COMMENT '指向产品表的id字段',
+  ptid int(11) DEFAULT NULL COMMENT '指向属性表的id字段',
   value varchar(255) DEFAULT NULL,
   PRIMARY KEY (id),
   CONSTRAINT fk_propertyvalue_property FOREIGN KEY (ptid) REFERENCES property (id),
@@ -54,7 +54,7 @@ CREATE TABLE propertyvalue (
 -- 产品图片表    存放产品图片信息，如产品页显示的5个图片
 CREATE TABLE productimage (
   id int(11) NOT NULL AUTO_INCREMENT,
-  pid int(11) DEFAULT NULL,
+  pid int(11) DEFAULT NULL COMMENT '外键pid 指向产品表的id字段',
   type varchar(255) DEFAULT NULL,
   PRIMARY KEY (id),
   CONSTRAINT fk_productimage_product FOREIGN KEY (pid) REFERENCES product (id)
@@ -64,8 +64,8 @@ CREATE TABLE productimage (
 CREATE TABLE review (
   id int(11) NOT NULL AUTO_INCREMENT,
   content varchar(4000) DEFAULT NULL,
-  uid int(11) DEFAULT NULL,
-  pid int(11) DEFAULT NULL,
+  uid int(11) DEFAULT NULL COMMENT '指向用户表的id字段',
+  pid int(11) DEFAULT NULL COMMENT '指向产品表的id字段',
   createDate datetime DEFAULT NULL,
   PRIMARY KEY (id),
   CONSTRAINT fk_review_product FOREIGN KEY (pid) REFERENCES product (id),
@@ -75,18 +75,18 @@ CREATE TABLE review (
 -- 订单表    存放订单信息，包括邮寄地址，电话号码等信息
 CREATE TABLE order_ (
   id int(11) NOT NULL AUTO_INCREMENT,
-  orderCode varchar(255) DEFAULT NULL,
-  address varchar(255) DEFAULT NULL,
-  post varchar(255) DEFAULT NULL,
-  receiver varchar(255) DEFAULT NULL,
-  mobile varchar(255) DEFAULT NULL,
-  userMessage varchar(255) DEFAULT NULL,
-  createDate datetime DEFAULT NULL,
-  payDate datetime DEFAULT NULL,
-  deliveryDate datetime DEFAULT NULL,
-  confirmDate datetime DEFAULT NULL,
-  uid int(11) DEFAULT NULL,
-  status varchar(255) DEFAULT NULL,
+  orderCode varchar(255) DEFAULT NULL COMMENT '订单号',
+  address varchar(255) DEFAULT NULL COMMENT '收货地址',
+  post varchar(255) DEFAULT NULL COMMENT '邮编',
+  receiver varchar(255) DEFAULT NULL COMMENT '收货人信息',
+  mobile varchar(255) DEFAULT NULL COMMENT '手机号码',
+  userMessage varchar(255) DEFAULT NULL COMMENT '用户备注信息',
+  createDate datetime DEFAULT NULL COMMENT '订单创建日期',
+  payDate datetime DEFAULT NULL COMMENT '支付日期',
+  deliveryDate datetime DEFAULT NULL COMMENT '发货日期',
+  confirmDate datetime DEFAULT NULL COMMENT '确认收货日期',
+  uid int(11) DEFAULT NULL  COMMENT '外键uid 指向用户表id字段',
+  status varchar(255) DEFAULT NULL COMMENT '订单状态',
   PRIMARY KEY (id),
   CONSTRAINT fk_order_user FOREIGN KEY (uid) REFERENCES user (id)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
@@ -94,10 +94,10 @@ CREATE TABLE order_ (
 -- 订单项表   存放订单项信息，包括购买产品种类，数量等
 CREATE TABLE orderitem (
   id int(11) NOT NULL AUTO_INCREMENT,
-  pid int(11) DEFAULT NULL,
-  oid int(11) DEFAULT NULL,
-  uid int(11) DEFAULT NULL,
-  number int(11) DEFAULT NULL,
+  pid int(11) DEFAULT NULL COMMENT '外键pid, 指向产品表id字段',
+  oid int(11) DEFAULT NULL COMMENT '外键oid 指向订单表id字段',
+  uid int(11) DEFAULT NULL COMMENT '指向用户表id字段',
+  number int(11) DEFAULT NULL COMMENT '购买数量',
   PRIMARY KEY (id),
   CONSTRAINT fk_orderitem_user FOREIGN KEY (uid) REFERENCES user (id),
   CONSTRAINT fk_orderitem_product FOREIGN KEY (pid) REFERENCES product (id),
